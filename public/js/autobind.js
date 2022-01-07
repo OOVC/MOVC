a6b17164160ccf5d8bf3ebb6176f50cc0e648790
Artemy <artegoser@gmail.com>
2022-01-07 15:37:13 +0300

    fix: autobind

--- public/js/autobind.js
+++ public/js/autobind.js
@@ -1,22 +1,26 @@
-window.addEventListener("load",async ()=>{
-    try{
-        document.getElementById("pass").value = localStorage.getItem("pass");
-    } catch{}
-    let idc = document.getElementById("idcinfo").content;
-    if(!idc) return;
-    let obj = await fetch('/api/country', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({idc})
-      });
-    obj = await obj.json();
-    obj.description = obj.srcdescription;
-    let keys = Object.keys(obj);
-    for(let i=0; i<keys.length; i++){
-        try{
-            document.getElementById(keys[i]).value = obj[keys[i]];
-        } catch {}
-    }
+window.addEventListener("load", async () => {
+  try {
+    document.getElementById("pass").value = localStorage.getItem("pass");
+  } catch {}
+  let idc = document.getElementById("idcinfo").content;
+  if (!idc) return;
+  let obj = await fetch("/api/country", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({ idc }),
+  });
+  obj = await obj.json();
+  let keys = Object.keys(obj);
+  for (let i = 0; i < keys.length; i++) {
+    try {
+      if (keys[i] === "description") {
+        if (obj.md) simplemde.value(obj.description);
+        else simplemde.value(obj.srcdescription);
+      } else {
+        document.getElementById(keys[i]).value = obj[keys[i]];
+      }
+    } catch {}
+  }
 });
